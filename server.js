272e237d15489e7bc777f3daa05642d18dcc883a
Chris Werkheiser <cjwerk@gmail.com>
2020-12-19 14:33:28 -0800

    inquirer

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer');
+
 const require = ('inquirer');
 const require = ('mysql2/promise');
 require('console.table');
@@ -7,10 +9,83 @@ const promptMessages = {
     viewAllEmployees: "View All Employees",
     viewByDepartment: "View All Employees By Department",
     viewByManager: "View All Employees By Manager",
-    addEMployee: "Add An Employee",
+    addEmployee: "Add An Employee",
     updateRole: "Update Employee Role",
     removeEmployee: "Remove An Employee",
     updateEmployeeManager: "Update Employees By Manager",
     viewAllRoles: "View All Rolls",
     exit: "Exit"
 };
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: '3306',
+
+    user: 'root',
+    
+    password: 'June52019!',
+    database: 'employees'
+
+});
+
+connection.connect(err => {
+    if(err) 
+        throw err;
+        prompt();
+});
+
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                promptMessages.viewAllEmployees,
+                promptMessages.viewByDepartment,
+                promptMessages.viewByManager,
+                promptMessages.viewAllRoles,
+                promptMessages.addEmployee,
+                promptMessages.removeEmployee,
+                promptMessages.updateRole,
+                promptMessages.exit
+            ]
+        })
+        .then(answer => {
+            console.log('answer', answer);
+            switch (answer.action) {
+                case promptMessages.viewAllEmployees:
+                    viewAllEmployees();
+                    break;
+
+                case promptMessages.viewByDepartment:
+                    viewByDepartment();
+                    break;
+
+                case promptMessages.viewByManager:
+                    viewByManager();
+                    break;
+
+                case promptMessages.addEmployee:
+                    addEmployee();
+                    break;
+
+                case promptMessages.removeEmployee:
+                    remove('delete');
+                    break;
+
+                case promptMessages.updateRole:
+                    remove('role');
+                    break;
+
+                case promptMessages.viewAllRoles:
+                    viewAllRoles();
+                    break;
+
+                case promptMessages.exit:
+                    connection.end();
+                    break;
+            }
+        });
+}
