799a0cb6f82de3991df9a747b5947e8cfda83ab0
Chris Werkheiser <cjwerk@gmail.com>
2020-12-19 14:16:08 -0800

    prompt messages

---
+++ server.js
@@ -0,0 +1,16 @@
+const require = ('inquirer');
+const require = ('mysql2/promise');
+require('console.table');
+
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEMployee: "Add An Employee",
+    updateRole: "Update Employee Role",
+    removeEmployee: "Remove An Employee",
+    updateEmployeeManager: "Update Employees By Manager",
+    viewAllRoles: "View All Rolls",
+    exit: "Exit"
+};
