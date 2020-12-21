0c8dc5205b44cc9c0e41dc3aa431f3b3eaffbf8c
Chris Werkheiser <cjwerk@gmail.com>
2020-12-20 17:05:15 -0800

    reformat

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXIST employees;
+DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
 CREATE TABLE department (
@@ -14,34 +14,37 @@ CREATE TABLE role (
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT UNSIGNED NOT NULL,
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 use employees;
 INSERT INTO department
-     (name)
-     VALUES
-('Operations'),
-('Analystics'),
-('Marketing'),
-('Executive');
-INSERT INTO role
-(title, salary, department_id)
+    (name)
 VALUES
-('Gereral Manager', 11000000, 1),
-('Coach', 400000, 1),
-('Team Lead Analyst', 15000000, 2),
-('Team Analyst', 8000000, 2),
-('Media Manager', 77000000, 3),
-('Media Specialist', 3000000, 3),
-('CEO', 45000000, 4),
-('CEO Assistant', 25000000, 4);
+    ('Operations'),
+    ('Analystics'),
+    ('Marketing'),
+    ('Executive');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('General Manager', 11000000, 1),
+    ('Coach', 4000000, 1),
+    ('Team Lead Analyst', 15000000, 2),
+    ('Team Analyst', 8000000, 2),
+    ('Media Manager', 7000000, 3),
+    ('Media Specialist', 3000000, 3),
+    ('CEO', 45000000, 4),
+    ('CEO Assistant', 25000000, 4);
 INSERT INTO employee
-(first_name, last_name, role_id, manager_id)
+    (first_name, last_name, role_id, manager_id)
 VALUES
 ('John', 'Stockton', 1, NULL),
 ('Peter', 'Parker', 2, 1),
